4caca3da261be2bb48eb433694df41d1e230bee5
Rohan859 <rohankundu859@gmail.com>
2024-01-13 01:40:45 +0530

    all made changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,20 +10,11 @@ class A
 
 }
 
-class B extends A
-{
-    
-    @Override
-    public String meth()
-    {
-        return "Method is overridden in Extendend class B";
-    }
-}
+
 public class Main
 {
     public static void main(String[] args)
     {
-        B obj=new B();
-        System.out.println(obj.meth());
+
     }
 }
