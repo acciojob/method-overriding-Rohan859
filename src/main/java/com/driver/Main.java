6acc047163c5366d37f9c44ab79263ddac871e01
Rohan859 <rohankundu859@gmail.com>
2024-01-13 01:10:15 +0530

    hi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,10 +12,7 @@ class A
 
 class B extends A
 {
-    B()
-    {
-        System.out.println(super.meth());
-    }
+    
     @Override
     public String meth()
     {
@@ -27,7 +24,6 @@ public class Main
     public static void main(String[] args)
     {
         B obj=new B();
-        System.out.println(obj.meth());
         System.out.println(obj.meth());
     }
 }
