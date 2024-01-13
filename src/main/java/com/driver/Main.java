81d2b8742af2a1b415e2b393627b6f6e583b7456
Rohan859 <rohankundu859@gmail.com>
2024-01-13 12:09:54 +0530

    separate file added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,12 @@
 package com.driver;
 
-class A
-{
-    public String meth()
-    {
-        return "Invoking method from class A";
-    }
-
-
-}
-
 
 public class Main
 {
     public static void main(String[] args)
     {
-
+        B obj=new B();
+        System.out.println(obj.meth());
+        System.out.println(obj.meth());
     }
 }
