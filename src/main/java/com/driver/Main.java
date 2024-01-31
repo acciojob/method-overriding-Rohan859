8a817eadbbff338d46ac6715a2276a4974cda2e0
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-01-31 20:30:48 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,6 +11,17 @@ public class Main
             return "Invoking method from class A";
         }
     }
+
+    public class B extends A
+    {
+        @Override
+        public String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+    
+    }
+    
     public static void main(String[] args)
     {
         B obj=new B();
