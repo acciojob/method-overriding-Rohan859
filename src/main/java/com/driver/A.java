aa4eb55b9e4a0cbd9fd4563a21cc2a6353c79e78
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-01-31 20:29:44 +0530

    Update A.java

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,9 +1,9 @@
-package com.driver;
+// package com.driver;
 
-public class A
-{
-    public String meth()
-    {
-        return "Invoking method from class A";
-    }
-}
+// public class A
+// {
+//     public String meth()
+//     {
+//         return "Invoking method from class A";
+//     }
+// }
