831af5c549e30803898b59d91e9d9bf964cce260
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-01-31 20:30:24 +0530

    Update B.java

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,12 +1,12 @@
-package com.driver;
-public class B extends A
-{
-    @Override
-    public String meth()
-    {
-        return "Method is overridden in Extendend class B";
-    }
+// package com.driver;
+// public class B extends A
+// {
+//     @Override
+//     public String meth()
+//     {
+//         return "Method is overridden in Extendend class B";
+//     }
 
-}
+// }
 
 
